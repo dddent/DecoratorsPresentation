c96e73c5842e50864ff2b7a4a4c4f552d215700b
David Heuschmann <heuschmann.d@gmail.com>
2016-11-28 17:14:34 +0100

    added example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ExampleModule } from './example.module/example.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/example.module/example.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ExampleComponent } from './example.component';
+
+@NgModule({
+  declarations: [ExampleComponent],
+  exports: [ExampleComponent]
+})
+export class ExampleModule { }
